d74bc719ea95abc08b9afadd8910b53788c0d4ba
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2024-05-13 20:46:39 -0400

    Created new user server component and journal page.

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,31 @@
-const NewUser = () => {
-    return <div>hi</div>
+import { prisma } from "@/utils/db"
+import { auth, currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const createNewUser = async () => {
+    const user = await currentUser()
+    console.log('user ', user)
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id as string,
+        }
+    })
+
+    if (!match) {
+        const nwUser = await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+            }
+        })
+    }
+
+    redirect('/journal')
+}
+
+const NewUser = async () => {
+    await createNewUser()
+    return <div>...loading</div>
 }
 
 export default NewUser
