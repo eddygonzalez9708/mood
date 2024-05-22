b296559567d91c40b8d26aad8f844c53c47a2116
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2024-05-21 21:40:33 -0400

    Fixed home and journal links on the dashboard.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,25 +1,28 @@
 import { UserButton } from "@clerk/nextjs"
-import Link from "next/link"
+import Link from "next/link";
 
 const links = [
-    { href: '/', label: 'Home' },
-    { href: '/journal', label: 'Journal' }
+    { href: '/', name: 'Home' },
+    { href: '/journal', name: 'Journals' }
 ]
 
 const DashboardLayout = ({ children }) => {
     return (
         <div className="h-screen w-screen relative">
-            <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">Mood</aside>
-            <div>
-                Mood
-            </div>
-            <ul>
-                {links.map(link => (
-                    <li key={link.href}>
-                        <Link href={link.href}>{link.label}</Link>
-                    </li>
-                ))}
-            </ul>
+            <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
+                <div>
+                    Mood
+                </div>
+                <div>
+                    <ul className="px-4">
+                        {links.map((link) => (
+                            <li key={link.name} className="text-xl my-4">
+                                <Link href={link.href}>{link.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </aside>
             <div className="ml-[200px] h-full">
                 <header className="h-[60px] border-b border-black/10">
                     <div className="h-full w-full px-6 flex items-center justify-end">
