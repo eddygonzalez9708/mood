22697800cef24538e145a6b702868eb3c75dc718
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2024-05-22 13:44:04 -0400

    Created a search form that uses Open AI and a vector database to answer questions about existing journal entries.

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,11 @@
 import { OpenAI } from "langchain/llms/openai"
-import {StructuredOutputParser} from 'langchain/output_parsers'
+import { StructuredOutputParser } from 'langchain/output_parsers'
 import z from 'zod'
 import { PromptTemplate } from 'langchain/prompts'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 
 const parser = StructuredOutputParser.fromZodSchema(
     z.object({
@@ -22,8 +26,8 @@ const parser = StructuredOutputParser.fromZodSchema(
         color: z
             .string()
             .describe(
-            'a hexadecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
-        )
+                'a hexadecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+            )
     })
 )
 
@@ -39,7 +43,7 @@ const getPrompt = async (content) => {
         entry: content,
     })
 
-    return input 
+    return input
 }
 
 export const analyze = async (content) => {
@@ -52,4 +56,26 @@ export const analyze = async (content) => {
     } catch (e) {
         console.log("error ", e)
     }
+}
+
+export const qa = async (question, entries) => {
+    const docs = entries.map(entry => {
+        return new Document({
+            pageContent: entry.content,
+            metadata: { id: entry.id, createdAt: entry.createdAt }
+        })
+    })
+
+    const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+    const chain = loadQARefineChain(model)
+    const embeddings = new OpenAIEmbeddings()
+    const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+    const relavantDocs = await store.similaritySearch(question)
+
+    const res = await chain.call({
+        input_documents: relavantDocs,
+        question,
+    })
+
+    return res.output_text
 }
